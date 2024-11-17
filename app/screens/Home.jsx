d7d5a7eac9db01e8c88b41430bb91393e39b1e2a
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-11-18 02:20:24 +0530

    default address context

--- app/screens/Home.jsx
+++ app/screens/Home.jsx
@@ -1,12 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { COLORS, SIZES } from "../constants/theme";
 import { SafeAreaView } from "react-native-safe-area-context";
 import pages from './page.style'
 import uidata from "../constants/uidata";
+import { UserReversedGeoCode } from '../context/UserReversedGeoCode';
 
 const Home = () => {
-  
+  const {address, setAddress} = useContext(UserReversedGeoCode);
+  console.log(address)
   return (
     <SafeAreaView>
       <View style={pages.viewOne}>
