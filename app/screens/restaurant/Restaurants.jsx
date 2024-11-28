aebd7f00423b0ff94fae0b3acec05c27c7c089ff
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2024-11-28 12:45:27 +0530

    restaurants rating

--- app/screens/restaurant/Restaurants.jsx
+++ app/screens/restaurant/Restaurants.jsx
@@ -4,9 +4,9 @@ import RestaurantNavigation from '../../navigation/RestaurantNavigation'
 import NetworkImage from '../../components/NetworkImage'
 import { COLORS, SIZES } from '../../constants/theme'
 import { useRoute } from '@react-navigation/native'
-import { TouchableOpacity } from 'react-native'
+import { TouchableOpacity } from 'react-native';
 import { Ionicons, MaterialCommunityIcons, AntDesign } from '@expo/vector-icons';
-
+import { RatingInput } from "react-native-stock-star-rating";
 const Restaurants = ({ navigation }) => {
 
   const route = useRoute();
@@ -25,9 +25,25 @@ const Restaurants = ({ navigation }) => {
           <MaterialCommunityIcons name='share-circle' size={40} color={COLORS.primary} />
         </TouchableOpacity>
         <NetworkImage data={item?.imageUrl} height={SIZES.height / 3.4} width={SIZES.width} radius={15} />
+        <View style={styles.rating}>
+          <View style={styles.innerRating}>
+            <RatingInput
+              rating={Number(item.rating)}
+              size={24}
+              maxStars={5}
+              setRating={item.rating}
+              bordered={false}
+              color={COLORS.lightWhite}
+            />
+            <TouchableOpacity style={styles.ratingBtn} onPress={() => { }}>
+              <Text style={styles.ratingText}>Rate Store</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
 
       <View style={{ height: 200 }}>
+
       </View>
 
       <View style={{ height: 600 }}>
@@ -55,4 +71,35 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: SIZES.xxLarge + 3,
   },
+
+  rating: {
+    position: 'absolute',
+    height: 50,
+    width: "100%",
+    bottom: -14,
+    zIndex: 999,
+    backgroundColor: COLORS.primary1,
+    justifyContent: 'center',
+    borderRadius: 15,
+  },
+
+  innerRating: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginHorizontal: 12
+  },
+
+  ratingBtn: {
+    borderColor: COLORS.lightWhite,
+    borderWidth: 1,
+    padding: 6,
+    borderRadius: 12,
+  },
+
+  ratingText: {
+    fontFamily: 'regular',
+    color: COLORS.lightWhite,
+    fontSize: 16,
+    fontWeight: 'medium',
+  }
 })
